c152ea8e2df90e7593a5318f17ff5cbf424a5350
ThomasDorland <Thomasdorland10@Live.nl>
2016-09-26 09:11:28 +0200

    Update Bot.php

--- src/Shotbow/ChatBot/Bot.php
+++ src/Shotbow/ChatBot/Bot.php
@@ -617,6 +617,7 @@ MySQL;
             }
         }
     }
+    
     protected function command_beta(Shotbow_ChatBot_User $sender, $arguments)
     {
         $message = 'Help us test 1.9! Connect to BETA.SHOTBOW.NET and try out our 1.9 gamemodes!';
